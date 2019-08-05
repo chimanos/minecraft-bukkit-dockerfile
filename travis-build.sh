18a180abb50a62a865fb26758c6e23966ab6fcc6
Romain Pamart <romain.pamart@yahoo.fr>
2019-08-05 22:54:11 +0200

    feat(Build): add check for latest tag

--- travis-build.sh
+++ travis-build.sh
@@ -3,10 +3,10 @@
 set -xe
 
 # Build
-if [[ -z "$DOCKERFILE" ]]; then
-	docker build --build-arg BUKKIT_VERSION=$TAG -t chimanos/mc-bukkit:$TAG .
+if [[ "$TAG" = "latest" ]]; then
+    docker build -t chimanos/mc-bukkit:$TAG .
 else
-	docker build -f $DOCKERFILE --build-arg BUKKIT_VERSION=$TAG -t chimanos/mc-bukkit:$TAG .
+    docker build --build-arg BUKKIT_VERSION=$TAG -t chimanos/mc-bukkit:$TAG .
 fi
 
 # Test
